7c1597606c906dfaaa8f3dc42bf1bd1a93faf10a
Michael Osuntuyi <mikosuntuyi@gmail.com>
2024-06-25 00:31:12 +0200

    Make website better fit for screens

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function createMessageBox(message) {
  
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "I knew you'd pick yes (¬‿¬), Text me what time on Friday!!";
+  question.innerHTML = "I knew you'd pick yes (¬‿¬), Text me what time for Friday!!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
